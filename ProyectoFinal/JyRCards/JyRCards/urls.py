074a867d49861a89d2b441677e640bfb881b15ef
jorgecba8989 <georgecba89@gmail.com>
2022-02-16 19:20:08 -0300

    agregado de navbar y cards de bootstrap

--- ProyectoFinal/JyRCards/JyRCards/urls.py
+++ ProyectoFinal/JyRCards/JyRCards/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 
+
 from JyRCards.views import contenidoHTML
 from JyRCards.templates import *
 
